766649233bbf5144b3b009c989c64cd7e394472d
nduka ojeh <nojeh@baneollc.com>
2024-07-22 21:56:27 -0400

    starting front end setup

--- infra-create/main.tf
+++ infra-create/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "tool" {
   ami                    = data.aws_ami.rhel9.image_id
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.tool-sg.id]
-#   iam_instance_profile   = aws_iam_instance_profile.instance-profile.name
+  iam_instance_profile   = aws_iam_instance_profile.instance-profile.name
 #
 #   instance_market_options {
 #     market_type = "spot"
