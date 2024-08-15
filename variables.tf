095dbac34ce735ed751ad6f439dc19514ba7d730
nduka ojeh <nojeh@baneollc.com>
2024-08-14 20:22:00 -0400

    adding minikube server to infra

--- variables.tf
+++ variables.tf
@@ -7,14 +7,25 @@ variable "tools" {
         "AdministratorAccess"
       ]
       ports = {}
+      volume_size = 20
     }
 
     vault = {
       instance_type = "t3.small"
-      policy_name   = []
+      policy_name = []
       ports = {
         vault = 8200
       }
+      volume_size = 20
+    }
+
+    minikube = {
+      instance_type = "t3.medium"
+      policy_name = []
+      ports = {
+        kube = 8443
+      }
+      volume_size = 30
     }
 
   }
